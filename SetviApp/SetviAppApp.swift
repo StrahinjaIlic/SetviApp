c7fd52f54b58f642267812a45e561ab32ff080b1
Strahinja Ilic <strahinjailic87@gmail.com>
2024-04-28 21:37:04 +0200

    Initial Commit

---
+++ SetviApp/SetviAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetviAppApp.swift
+//  SetviApp
+//
+//  Created by Strahinja Ilic on 28.4.24..
+//
+
+import SwiftUI
+
+@main
+struct SetviAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
